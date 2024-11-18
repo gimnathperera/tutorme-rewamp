aa5f658d64f07cc517ef8d33ce6b2268ea313f3e
gimnathperera <gimnathperera@gmail.com>
2024-11-18 20:52:04 +0530

    feat: add RHF to signup form

--- src/components/auth/form-sign-up/index.tsx
+++ src/components/auth/form-sign-up/index.tsx
@@ -1,47 +1,62 @@
 import InputText from "@/components/shared/input-text";
 import SubmitButton from "@/components/shared/submit-button";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { SignUpSchema, signUpSchema } from "./schema";
 
 type Props = {
   onLoginClick: () => void;
 };
 
 const FormSignUp = ({ onLoginClick }: Props) => {
-  return (
-    <form action="#">
-      <div className="space-y-4">
-        <InputText
-          label="Email"
-          name="email"
-          placeholder="jhon@xyz.com"
-          type="email"
-        />
-        <InputText
-          label="Password"
-          name="password"
-          placeholder="*******"
-          type="password"
-        />
-        <InputText
-          label="Confirm Password"
-          placeholder="*******"
-          type="password"
-          name="confirmPassword"
-        />
-      </div>
-      <div className="space-y-2 mt-8">
-        <SubmitButton title="Register" type="submit" />
+  const signupForm = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {} as SignUpSchema,
+    mode: "onChange",
+  });
 
-        <div className="text-center">
-          <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-            Already have an account?{" "}
-            <span className="text-blue cursor-pointer" onClick={onLoginClick}>
-              Login
-            </span>
-          </p>
+  const onSubmit = (data: SignUpSchema) => {
+    console.log("Form Submitted", data);
+  };
+
+  return (
+    <FormProvider {...signupForm}>
+      <form onSubmit={signupForm.handleSubmit(onSubmit)}>
+        <div className="space-y-4">
+          <InputText
+            label="Email"
+            name="email"
+            placeholder="jhon@xyz.com"
+            type="email"
+          />
+          <InputText
+            label="Password"
+            name="password"
+            placeholder="*******"
+            type="password"
+          />
+          <InputText
+            label="Confirm Password"
+            placeholder="*******"
+            type="password"
+            name="confirmPassword"
+          />
         </div>
-      </div>
-    </form>
+        <div className="space-y-2 mt-8">
+          <SubmitButton title="Register" type="submit" />
+
+          <div className="text-center">
+            <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+              Already have an account?{" "}
+              <span className="text-blue cursor-pointer" onClick={onLoginClick}>
+                Login
+              </span>
+            </p>
+          </div>
+        </div>
+      </form>
+    </FormProvider>
   );
 };
 
---
+++ src/components/auth/form-sign-up/schema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const signUpSchema = z
+  .object({
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long" }),
+    confirmPassword: z.string().min(6, {
+      message: "Confirm Password must be at least 6 characters long",
+    }),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.confirmPassword;
+    },
+    {
+      message: "Passwords don't match",
+      path: ["confirmPassword"],
+    }
+  );
+
+export type SignUpSchema = z.infer<typeof signUpSchema>;
