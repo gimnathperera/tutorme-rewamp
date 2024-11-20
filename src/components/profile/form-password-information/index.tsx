dd3686db56ec0fcf1e4d5ffae167f5d1c676606a
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-11-20 16:59:47 +0530

    feat: update password Information section

--- src/components/profile/form-password-information/index.tsx
+++ src/components/profile/form-password-information/index.tsx
@@ -42,22 +42,6 @@ const FormPasswordInfo: FC = () => {
                 type="password"
               />
             </div>
-            {/* <div className="col-span-6 sm:col-span-3">
-              <InputText
-                label="New password"
-                name="New password"
-                placeholder="*******"
-                type="password"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-3">
-              <InputText
-                label="Confirm password"
-                name="Confirm password"
-                placeholder="*******"
-                type="password"
-              />
-            </div> */}
             <div className="col-span-6 sm:col-full">
               <button
                 className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-5 text-center   "
