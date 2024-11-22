69eab70e822a908a46bec7775fa6aa2fee7607b8
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-11-22 21:26:48 +0530

    feat: add rhf for General information form

---
+++ src/components/profile/form-language-time/schema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const passwordInfoSchema = z
+  .object({
+    currentPassword: z.string().min(1, { message: "Password is required" }),
+    newPassword: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long" })
+      .regex(/[a-z]/, { message: "Must include at least one lowercase letter" })
+      .regex(/[A-Z]/, { message: "Must include at least one uppercase letter" })
+      .regex(/[0-9]/, { message: "Must include at least one number" })
+      .regex(/[#@$&]/, {
+        message: "Must include at least one special symbol (#, @, $, &)",
+      }),
+    confirmPassword: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long" }),
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Confirm Password must match New Password",
+    path: ["confirmPassword"],
+  });
+
+export type PasswordInfoSchema = z.infer<typeof passwordInfoSchema>;
