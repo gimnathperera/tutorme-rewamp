7068eb888e4265d418b40cc452c155a8ba21e677
gimnathperera <gimnathperera@gmail.com>
2024-11-22 06:37:46 +0530

    feat: remove unwanted code

--- src/components/profile/form-password-information/index.tsx
+++ src/components/profile/form-password-information/index.tsx
@@ -21,35 +21,33 @@ const FormPasswordInfo: FC = () => {
       <h3 className="mb-4 text-xl font-semibold ">Password information</h3>
       <FormProvider {...passwordInfoForm}>
         <form onSubmit={passwordInfoForm.handleSubmit(onSubmit)}>
-          <div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols- gap-6">
-              <InputText
-                label="Current password"
-                name="currentPassword"
-                placeholder="*******"
-                type="password"
-              />
-              <InputText
-                label="New password"
-                name="newPassword"
-                placeholder="*******"
-                type="password"
-              />
-              <InputText
-                label="Confirm password"
-                name="confirmPassword"
-                placeholder="*******"
-                type="password"
-              />
-            </div>
-            <div className="col-span-6 sm:col-full">
-              <button
-                className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-5 text-center   "
-                type="submit"
-              >
-                Save all
-              </button>
-            </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols- gap-6">
+            <InputText
+              label="Current password"
+              name="currentPassword"
+              placeholder="*******"
+              type="password"
+            />
+            <InputText
+              label="New password"
+              name="newPassword"
+              placeholder="*******"
+              type="password"
+            />
+            <InputText
+              label="Confirm password"
+              name="confirmPassword"
+              placeholder="*******"
+              type="password"
+            />
+          </div>
+          <div className="col-span-6 sm:col-full">
+            <button
+              className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-5 text-center   "
+              type="submit"
+            >
+              Save all
+            </button>
           </div>
         </form>
       </FormProvider>
