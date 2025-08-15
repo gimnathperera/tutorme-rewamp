698ef7ba7ef401cdf30502e919f93fef6f2f91e7
Jayathu Shehan Dewmina <jayathushehan@gmail.com>
2025-08-15 17:21:31 +0530

    Revert "feat(tuitionAssignment): created the selected page"
    This reverts commit cd3bd61bceb4bfa46d6b13b46c4bde2a40260c7a.

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -73,18 +73,4 @@ const CardFooter = React.forwardRef<
 ))
 CardFooter.displayName = "CardFooter"
 
-function CardAction({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-action"
-      className={cn(
-        "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
-        className
-      )}
-      {...props}
-    />
-  )
-}
-CardAction.displayName = "CardAction"
-
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent, CardAction }
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
