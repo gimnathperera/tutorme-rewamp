48e52e940b10dc3c5ed5a226741e3fb70f0a42af
Jayathu Shehan Dewmina <jayathushehan@gmail.com>
2025-09-02 15:59:35 +0530

    fix(tuition assignment): page title

--- src/app/tuition-assignments/page.tsx
+++ src/app/tuition-assignments/page.tsx
@@ -4,7 +4,7 @@ import Filter from "./components/filter";
 const TuitionAssignments = () => {
   return (
     <div className="min-h-screen bg-gray-100 px-4 sm:px-8 md:px-16 lg:px-32 py-10 rounded-2xl">
-      <p className="font-extrabold text-3xl sm:text-4xl py-6 text-center">Find A Tutor</p>
+      <p className="font-extrabold text-3xl sm:text-4xl py-6 text-center">Find An Assignment</p>
       <Filter />
     </div>
   );
