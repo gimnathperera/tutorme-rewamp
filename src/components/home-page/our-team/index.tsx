d15bb49601f7a152c2c587a965cd27ea9c128ae6
piyumikajayasiri <malini.softvilmedia@gmail.com>
2025-10-16 15:34:09 +0530

    fix: changing dummy data to real data

--- src/components/home-page/our-team/index.tsx
+++ src/components/home-page/our-team/index.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
-import TeamImage from "../../../../public/images/team/teamimg.png";
+import TutorImage from "../../../../public/images/banner/banner2.jpg";
 
 const OurTeam = () => {
   return (
     <div className="mx-auto max-w-7xl sm:py-4 lg:px-8 m-32">
       <h2 className="text-4xl sm:text-65xl font-bold text-center">
-        Our team belives you deserve <br /> only the best.
+        Our team belives you deserve <br /> only the best learning experience.
       </h2>
       <h3 className="text-2xl font-medium text-center pt-10 opacity-50">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br />{" "}
-        eiusmod tempor.
+        We&apos;re dedicated to helping students and tutors connect through a
+        trusted, easy-to-use platform.
+        <br />
+        Every learner deserves quality education — and every tutor deserves
+        recognition for their effort.
       </h3>
       <div className="grid grid-cols-1 my-16">
-        <Image src={TeamImage} alt="office-image" height={684} width={1296} />
+        <Image src={TutorImage} alt="office-image" height={684} width={1296} />
       </div>
     </div>
   );
