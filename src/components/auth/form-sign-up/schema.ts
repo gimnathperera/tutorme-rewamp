bd143a7087631f71800863b0a9d115b1ec7c3f10
LGRMHV Rathnayaka <90521652+Hirantha12@users.noreply.github.com>
2025-10-28 15:47:31 +0530

    fix: enhance validation for sign-up form fields

--- src/components/auth/form-sign-up/schema.ts
+++ src/components/auth/form-sign-up/schema.ts
@@ -2,15 +2,22 @@ import { z } from "zod";
 
 export const signUpSchema = z
   .object({
-    name: z.string().min(1, "Full Name is required"),
-    email: z.string().email({ message: "Invalid email address" }),
+    name: z.string().nonempty("Full Name is required"),
+
+    email: z
+      .string()
+      .nonempty("Email is required.")
+      .email({ message: "Invalid email address" }),
+
     password: z
       .string()
+      .nonempty("Password is required.")
       .min(8, { message: "Password must be at least 8 characters long" })
       .max(12, { message: "Password cannot exceed 12 characters" }),
 
     confirmPassword: z
       .string()
+      .nonempty("Please confirm your password.")
       .min(8, {
         message: "Confirm Password must be at least 8 characters long",
       })
