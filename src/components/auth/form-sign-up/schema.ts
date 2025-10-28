61e85b3e4f072b0e3ef261e8a1a68cc216593d24
LGRMHV Rathnayaka <90521652+Hirantha12@users.noreply.github.com>
2025-10-28 14:55:57 +0530

    fix(auth): enforce 8-12 character password length

--- src/components/auth/form-sign-up/schema.ts
+++ src/components/auth/form-sign-up/schema.ts
@@ -6,10 +6,15 @@ export const signUpSchema = z
     email: z.string().email({ message: "Invalid email address" }),
     password: z
       .string()
-      .min(6, { message: "Password must be at least 6 characters long" }),
-    confirmPassword: z.string().min(6, {
-      message: "Confirm Password must be at least 6 characters long",
-    }),
+      .min(8, { message: "Password must be at least 8 characters long" })
+      .max(12, { message: "Password cannot exceed 12 characters" }),
+
+    confirmPassword: z
+      .string()
+      .min(8, {
+        message: "Confirm Password must be at least 8 characters long",
+      })
+      .max(12, { message: "Password cannot exceed 12 characters" }),
   })
   .refine(
     (data) => {
