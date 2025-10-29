ac87fdba19060dc271294b8ae5c2e5e06e38b152
LGRMHV Rathnayaka <90521652+Hirantha12@users.noreply.github.com>
2025-10-29 09:56:13 +0530

    fix(auth): simplify confirm password validation logic

--- src/components/auth/form-sign-up/schema.ts
+++ src/components/auth/form-sign-up/schema.ts
@@ -25,23 +25,7 @@ export const signUpSchema = z
         }
       }),
 
-    confirmPassword: z
-      .string()
-      .nonempty("Please confirm your password.")
-      .min(8, {
-        message: "Confirm Password must be at least 8 characters long",
-      })
-      .max(12, { message: "Password cannot exceed 12 characters" })
-      .superRefine((val, ctx) => {
-        const hasLetter = /[a-zA-Z]/.test(val);
-        const hasNumber = /\d/.test(val);
-        if (!hasLetter || !hasNumber) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: "password must contain at least 1 letter and 1 number",
-          });
-        }
-      }),
+    confirmPassword: z.string().nonempty("Please confirm your password."),
   })
   .refine(
     (data) => {
