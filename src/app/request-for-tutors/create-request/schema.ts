5b620fbbc7ce39cc2c95daaa44ef51ab533c28d2
Jayathu Shehan Dewmina <106993037+JayathuShehan@users.noreply.github.com>
2026-02-24 10:46:15 +0530

    feat: Add Zod schema and initial form values for creating tutor requests.

--- src/app/request-for-tutors/create-request/schema.ts
+++ src/app/request-for-tutors/create-request/schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createRequestTutorSchema = z.object({
   name: z.string().min(1, "Name is required"),
-  email: z.string().min(1, "Enter a valid email").email("Enter a valid email"),
+  email: z.string().min(1, "Email is required").email("Enter a valid email"),
   city: z.string().min(1, "City is required"),
   district: z.string().min(1, "District is required"),
   phoneNumber: z
@@ -12,16 +12,16 @@ export const createRequestTutorSchema = z.object({
     .refine((val) => val.length === 10, {
       message: "Phone number must be exactly 10 digits",
     }),
-  medium: z.string().nonempty("Please select a medium"),
-  grade: z.string().nonempty("Please select a grade"),
+  medium: z.string().nonempty("Medium is required"),
+  grade: z.string().nonempty("Grade is required"),
   tutors: z
     .array(
       z.object({
-        subject: z.string().nonempty("Please select a subject"),
+        subject: z.string().nonempty("Subject is required"),
         assignedTutor: z.string().optional().default(""),
         duration: z.string().nonempty("Duration is required"),
         frequency: z.string().nonempty("Frequency is required"),
-        preferredTutorType: z.string().nonempty("Please select a tutor type"),
+        preferredTutorType: z.string().nonempty("Tutor type is required"),
       })
     )
     .min(1, "Tutor count is required"),
