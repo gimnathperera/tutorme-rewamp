e9426f2e0512fc8b072f3c32c3e5dd1b9aa52103
Thevindu Guruge <gurugetnm@gmail.com>
2026-04-10 10:42:36 +0530

    refactor: update text to specify 'Hourly Tuition Rates' in TuitionRatesPage

--- src/app/tuition-rates/page.tsx
+++ src/app/tuition-rates/page.tsx
@@ -6,11 +6,10 @@ const TuitionRatesPage = () => {
     <div className="m-10">
       <div className="mx-auto max-w-7xl py-4 m-3">
         <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold text-center">
-          Check the Tuition Rates and Let&apos;s Get Learning!
+          Check the Hourly Tuition Rates and Let&apos;s Get Learning!
         </h2>
         <h3 className="text-xl sm:text-2xl font-medium text-center pt-4 sm:pt-10 opacity-50 mb-12">
-          Explore courses and resources just for you! See the relative tuition
-          rates to begin
+          Explore courses and resources just for you! See the relative hourly tuition rates to begin
           <br className="hidden sm:block" />
           an exciting and personalized learning adventure!
         </h3>
