8c23f2f903cae6332721f8ba45529f397f27278e
LGRMHV Rathnayaka <90521652+Hirantha12@users.noreply.github.com>
2026-05-08 16:27:30 +0530

    fix(TM-844): add trim and no-space validation to login email and password fields

--- src/components/auth/form-login/schema.ts
+++ src/components/auth/form-login/schema.ts
@@ -3,12 +3,15 @@ import { z } from "zod";
 export const loginSchema = z.object({
   email: z
     .string()
+    .trim()
     .min(1, { message: "Email is not allowed to be empty" })
     .email({ message: "Invalid email address" }),
 
   password: z
     .string()
+    .trim()
     .min(1, { message: "Password is not allowed to be empty" })
+    .regex(/^\S+$/, { message: "Password must not contain spaces." })
     .min(8, { message: "Password must be at least 8 characters long" }),
 });
 
