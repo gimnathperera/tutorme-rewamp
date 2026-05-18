9a7a3c691ddecb196abf1f39112d2d36f97f595d
LGRMHV Rathnayaka <90521652+Hirantha12@users.noreply.github.com>
2026-05-18 10:02:37 +0530

    feat: add Home as first item in desktop navbar navigation

--- src/components/shared/navbar/header.tsx
+++ src/components/shared/navbar/header.tsx
@@ -20,6 +20,7 @@ interface NavigationItem {
 }
 
 const navigation: NavigationItem[] = [
+  { name: "Home", href: "/" },
   { name: "Request for Tutor", href: "/request-for-tutors" },
   { name: "Register as a Tutor", href: "/register-tutor" },
   {
