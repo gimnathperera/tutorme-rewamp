590dc3332779f32c621bf6ee164ccfb88407ea13
piyumikajayasiri <malini.softvilmedia@gmail.com>
2026-05-19 16:32:41 +0530

    fix: Added infinite-scroll pagination to the Grades & Subjects page

--- src/types/request-types.ts
+++ src/types/request-types.ts
@@ -121,6 +121,8 @@ export type FetchPapersRequest = {
 export type FetchGradesRequest = {
   page: number;
   limit: number;
+  title?: string;
+  sortBy?: string;
 };
 export type FetchTuitionRatesRequest = {
   tuitionRateId?: string;
