0a4a5322e3136f6118ef368795f0d7ecc58099d4
LGRMHV Rathnayaka <90521652+Hirantha12@users.noreply.github.com>
2026-06-15 15:27:08 +0530

    fix: padding_size_of_tuition rates_mobile

--- src/app/[locale]/tuition-rates/page.tsx
+++ src/app/[locale]/tuition-rates/page.tsx
@@ -9,7 +9,7 @@ const TuitionRatesPage = () => {
 
   return (
     <div className="px-4 lg:px-8">
-      <div className="mx-auto max-w-7xl pb-6 sm:py-10">
+      <div className="mx-auto max-w-7xl py-6 sm:py-10">
         <h2 className="text-3xl font-bold text-center">{t("pageHeading")}</h2>
         <h3 className="mx-auto mt-3 max-w-2xl px-4 sm:px-0 text-sm md:text-base font-normal text-center opacity-50 mb-12">
           {t("pageSubheading")}
